746a4f5fdb53a778c6bd3e37e108b790916ca8c2
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-09-19 22:42:56 +0100

    Added pause UI

---
+++ Assets/Scripts/TenSecondsReplay/PauseUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TenSecondsReplay
+{
+    public class PauseUI : MonoBehaviour
+    {
+        [SerializeField] private Button menuButton, quitButton;
+        [SerializeField] private GameObject holder;
+        
+        private bool isPaused;
+        
+        private void Awake()
+        {
+            menuButton.onClick.AddListener(OnMenuButton);
+            quitButton.onClick.AddListener(OnQuitButton);
+        }
+
+        private void OnQuitButton()
+        {
+            Application.Quit();
+        }
+
+        void DeterminePaused()
+        {
+            holder.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0f : 1f;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                isPaused = !isPaused;
+                DeterminePaused();
+            }
+        }
+
+        private void OnBackButton()
+        {
+            isPaused = false;
+            DeterminePaused();
+        }
+
+        private void OnMenuButton()
+        {
+            
+        }
+    }
+}
