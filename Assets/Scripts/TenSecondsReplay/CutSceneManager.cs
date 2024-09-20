3e7487417d3052f9171bd39d5f8cf50952ad9574
diandrakathyusha <116161960+diandrakathyusha@users.noreply.github.com>
2024-09-20 16:51:35 +0100

    Implemented CutScene
    i added the cutscene + some simple animation

---
+++ Assets/Scripts/TenSecondsReplay/CutSceneManager.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneManager : MonoBehaviour
+{
+    public List<GameObject> cutsceneObjects; // List of canvases or game objects to display in sequence
+    public string mainGameSceneName = "Main"; // Name of the main game scene to load
+
+    private int currentObjectIndex = 0;
+
+    void Start()
+    {
+        // Ensure all objects are hidden except the first one
+        for (int i = 0; i < cutsceneObjects.Count; i++)
+        {
+            cutsceneObjects[i].SetActive(i == 0); // Only activate the first object
+        }
+    }
+
+    void Update()
+    {
+        // Check if the user presses the space bar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextObject();
+        }
+    }
+
+    void DisplayNextObject()
+    {
+        // Hide the current object
+        cutsceneObjects[currentObjectIndex].SetActive(false);
+
+        // Move to the next object
+        currentObjectIndex++;
+
+        // If there are more objects to show, display the next one
+        if (currentObjectIndex < cutsceneObjects.Count)
+        {
+            cutsceneObjects[currentObjectIndex].SetActive(true);
+        }
+        else
+        {
+            // If we've reached the end, load the main game scene
+            LoadMainGameScene();
+        }
+    }
+
+    void LoadMainGameScene()
+    {
+        // Load the main game scene
+        SceneManager.LoadScene(mainGameSceneName);
+        Debug.Log("play main scene");
+    }
+}
